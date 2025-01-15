57a7cb9d6416ba6a6fbda49101d37c3887c4e0fc
RajaMusawir <gta5kingmaster@gmail.com>
2025-01-15 20:04:59 +0500

    problem

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,30 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
+module.exports = (async () => {
+    const {
+      resolver: {
+        sourceExts,
+        assetExts
+      }
+    } = await getDefaultConfig();
+  
+    return {
+      transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+        getTransformOptions: async () => ({
+          transform: {
+            experimentalImportSupport: false,
+            inlineRequires: true,
+          },
+        }),
+      },
+      resolver: {
+        assetExts: assetExts.filter(ext => ext !== 'svg'),
+        sourceExts: [...sourceExts, 'svg']
+      }
+    }
+  })();
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
